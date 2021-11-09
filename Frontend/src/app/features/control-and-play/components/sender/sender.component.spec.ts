834a13d8e270d946e56da5e3a4611528569988ee
Dorin Potorac <dopoto@gmail.com>
2021-11-09 06:35:57 +0200

    Fix tests. Re-enable pipeline test runner

--- Frontend/src/app/features/control-and-play/components/sender/sender.component.spec.ts
+++ Frontend/src/app/features/control-and-play/components/sender/sender.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/state/app.state';
 
 import { SenderComponent } from './sender.component';
 
@@ -6,10 +8,15 @@ describe('SenderComponent', () => {
     let component: SenderComponent;
     let fixture: ComponentFixture<SenderComponent>;
 
+    let store: MockStore;
+    const initialState = {} as AppState;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [SenderComponent],
+            providers: [provideMockStore({ initialState })],
         }).compileComponents();
+        store = TestBed.inject(MockStore);
     });
 
     beforeEach(() => {
